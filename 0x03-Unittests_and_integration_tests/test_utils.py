439da8d2945fcb2f1109952560e1fa09cfa71c03
daveyhmariam <daveyhmariam@gmail.com>
2024-07-30 01:07:31 +0300

    test memoixation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,21 +55,20 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """Tests the `memoize` function."""
-
     def test_memoize(self) -> None:
         """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
 
-            @utils.memoize
+            @memoize
             def a_property(self):
                 return self.a_method()
         with patch.object(
                 TestClass,
                 "a_method",
                 return_value=lambda: 42,
-        ) as memo_fxn:
+                ) as memo_fxn:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
