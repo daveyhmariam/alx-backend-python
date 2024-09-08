d539ce91d7cd1e4109cc7ae862f68497bab67335
daveyhmariam <daveyhmariam@gmail.com>
2024-09-08 12:14:50 +0300

    type annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Type annotation
+"""
+
+
+def to_str(n: float) -> str:
+    """Type annotation
+
+    Args:
+        n (float):
+
+    Returns:
+        str:
+    """
+    return str(n)
