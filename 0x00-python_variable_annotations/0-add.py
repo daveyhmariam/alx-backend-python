1c3b889e4e546b3eb39d70f91d425631b89a6491
daveyhmariam <daveyhmariam@gmail.com>
2024-09-08 12:01:53 +0300

    type annotaion

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Type annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Type annotation
+
+    Args:
+        a (float): operand
+        b (float): operand
+
+    Returns:
+        float: summation
+    """
+    return a + b
