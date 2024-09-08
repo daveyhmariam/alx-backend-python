5673e862ecce917af06faac7fa2dc3999b92319c
daveyhmariam <daveyhmariam@gmail.com>
2024-09-08 12:11:30 +0300

    annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Type annotation
+"""
+
+
+def floor(n: float) -> int:
+    """Type annotation
+
+    Args:
+        n (float): input
+
+    Returns:
+        int: return floor of n
+    """
+    return int(n)
