7b6aaca241b2ca51deca77400e4211dc1f8486e3
daveyhmariam <daveyhmariam@gmail.com>
2024-09-08 12:06:26 +0300

    annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Type annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type annotation
+
+    Args:
+        str1 (str): string 1
+        str2 (str): strnging2
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
