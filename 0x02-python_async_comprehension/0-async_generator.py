acb375bb9332b0456a6111a37a38af68f3228019
daveyhmariam <daveyhmariam@gmail.com>
2024-09-09 11:07:17 +0300

    async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pyrthon3
+"""Asyncronous comprehension
+"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """async comprehension
+
+    Yields:
+        Generator[float, int]:
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
