170daaae38746919171365aee56cd9806286b5d7
daveyhmariam <daveyhmariam@gmail.com>
2024-09-09 11:17:22 +0300

    doc

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,9 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Async comprehension
+
+    Returns:
+        List[float]:
+    """
     return [num async for num in async_generator()]
